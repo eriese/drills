140c2f8013ee20fdd95e37ebe035d2d9e50edda6
Enoch Riese <enoch.riese@gmail.com>
2013-09-25 09:55:00 -0400

    elaborated on first date

--- first_date.rb
+++ first_date.rb
@@ -4,11 +4,14 @@ puts "Do you plan on having children? (y/n)"
 ans = gets.chomp!
 if ans == "y" then
   puts "What will be their names?"
+else puts "Yeah, they're not for everyone."
 end
 if age.to_i > 30 then
   puts "Are you married? (y,n)"
   ans2 = gets.chomp!
+else puts "That's real."
 end
 if ans2 == "y"
   puts "Do you plan on getting married?"
+else puts "Cool, me neither."
 end
