63a428b5d258d6340f426b682b6b9890d9c315cf
Enoch Riese <enoch.riese@gmail.com>
2013-09-25 10:13:36 -0400

    perfected first date

--- first_date.rb
+++ first_date.rb
@@ -4,14 +4,22 @@ puts "Do you plan on having children? (y/n)"
 ans = gets.chomp!
 if ans == "y" then
   puts "What will be their names?"
+  kids = gets.chomp!
+  puts "Cute."
 else puts "Yeah, they're not for everyone."
 end
 if age.to_i > 30 then
   puts "Are you married? (y,n)"
   ans2 = gets.chomp!
-else puts "That's real."
+  if ans2 == "n"
+    puts "That's real."
+    puts "Do you plan on getting married?(y,n)"
+    plans = gets.chomp!
+    if plans == "y"
+    puts "That's nice."
+    else puts "Cool, me neither."
+    end
+  else puts "Cool."
+  end
 end
-if ans2 == "n"
-  puts "Do you plan on getting married?"
-else puts "Cool, me neither."
-end
+
